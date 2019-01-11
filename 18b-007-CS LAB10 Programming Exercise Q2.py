e71938bce6e2e3c96bbdbe08735af43ca3b07496
Jawwad18B <45209922+Jawwad18B@users.noreply.github.com>
2019-01-11 12:23:23 +0500

    Add files via upload

---
+++ 18b-007-CS LAB10 Programming Exercise Q2.py
@@ -0,0 +1,6 @@
+print('Jawwad 18b-007-CS(A)')
+print('Lab 10 Programming exercise Q2')
+phonedirectory = {'Charlie':'000423059','Timothy':'4534635','Dom':'54565646','Paul':'2454543','Xan':'2345235','Blake':'93948456','Aaron':'3253425','Denice':'4543643','Bob':'3532563','James':'2354666'}
+del phonedirectory['Charlie']
+print(len(phonedirectory))    
+    
